5bbe3bc082826ebafca5a316d1e98624424cabf3
schengen <chris.a.engen@gmail.com>
2016-03-02 15:32:22 +0100

    Create new ReservationList container

---
+++ src/constants/Reservations.js
@@ -0,0 +1,12 @@
+export const reservationsData = [
+  {
+    reservationId: 0,
+    reserverId: 0,
+    guestIds: [0, 1],
+    roomId: 1,
+    date: {
+      start: '21/05/2016',
+      end: '15/06/2016',
+    },
+  },
+];
---
+++ src/containers/ReservationList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const mapStoreToProps = () => {
+  return {};
+};
+
+const mapDispatchToProps = () => {
+  return {};
+};
+
+const _ReservationList = () => {
+  return (
+
+  )
+};
+
+export default ReservationList = connect(
+  mapStoreToProps,
+  mapDispatchToProps
+)(_ReservationList);
