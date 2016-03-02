8af0f0fe3ac04868f80987d61bba50d8e3cbaae8
schengen <chris.a.engen@gmail.com>
2016-03-02 15:31:27 +0100

    Add users reducer and update reservations

--- src/reducers/reservations.js
+++ src/reducers/reservations.js
@@ -1,6 +1,5 @@
 import * as types from '../constants/ActionTypes';
 
-
 const reservation = (state, action) => {
   switch (action.type) {
     case types.ADD_RESERVATION:
---
+++ src/reducers/users.js
@@ -0,0 +1,35 @@
+const initialState = [
+  {
+    id: 0,
+    firstName: 'Nora',
+    lastName: 'Lindtner',
+    img: '',
+  },
+  {
+    id: 1,
+    firstName: 'Christian',
+    lastName: 'Engen',
+    img: '',
+  },
+  {
+    id: 2,
+    firstName: 'Anne Marie',
+    lastName: 'Ottersen',
+    img: '',
+  },
+  {
+    id: 0,
+    firstName: 'Lasse',
+    lastName: 'Lindtner',
+    img: '',
+  },
+];
+
+const users = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default users;
